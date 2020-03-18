6676e950a56c59b2d636ecacfc9757a9280fdbac
Sharvanand <thirusharwan@gmail.com>
2020-03-17 22:49:34 -0400

    Git Ignore

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const validator = require("validator");
 const fs = require("fs");
 const getNotes = require("./notes");
 
 const msg = getNotes();
 console.log(msg);
+console.log(validator.isEmail("thirusharwan@gmail.com"));
